f27f037c6f21f801b4ffba6e1e19cb795ffa464e
medoledo <medoledo144@gmail.com>
2025-08-07 10:18:12 +0300

    fixing

--- backend/settings.py
+++ backend/settings.py
@@ -166,9 +166,6 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 
 
 MEDIA_URL = '/media/'  # URL to serve media files
